75dbc5eacab2cb38866ebbe196ce3cb4869db1df
Quezka <132306310+Quezka@users.noreply.github.com>
2025-04-01 08:19:41 +0200

    changed README

--- 19-03-2025/giocoSwitch_Domashenko.cpp
+++ 19-03-2025/giocoSwitch_Domashenko.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 /*
@@ -10,31 +11,61 @@ a ogni turno un giocatore ha tre possibilità:
     3 – estrarre due numeri casuali tra 1 e 5 in questo caso il prodotto tra questi due numeri verrà sommato al punteggio del giocatore.
 Se un giocatore supera 100 ha perso, se un giocatore si ferma e l’altro supera il suo punteggio senza fare più di 100 punti ha perso.
 */
+
 int main() {
-    int player1 = 0, player2 = 0, choice, num1, num2, turn = 0;
+    int player1 = 0, player2 = 0, choice, num1, num2, turn = 0, wins = -1;
+    bool stop1 = false, stop2 = false;
 
     srand(time(NULL));
 
-    system("cls");
+    while (wins < 0) {
+        system("cls");
 
-    while (!(player1 >= 100) and !(player2 >= 100)) {
         if (turn == 0) {
-            cout << "PLAYER 1 - " << player1 << "\n";
+            cout << "PLAYER 1 - " << player1 << " ~ " << "PLAYER 2 - " << player2 << "\n";
         }
         else {
-            cout << "PLAYER 2 - " << player2 << "\n";
+            cout << "PLAYER 2 - " << player2 << " ~ " << "PLAYER 1 - " << player1 << "\n";
         }
-        cout << "Inserisci una scelta:\n    1 - Rimani fermo (non estrai numeri);\n    2 - Estrai numero casuale tra 5 e 10;\n    3 - Estrai due numeri casuali tra 1 e 5;\nScegli >> ";
 
-        cin >> choice;
+        if (turn == 0 and stop1 == true) {
+            choice = 1;
+            turn = 1;
+        }
+        else if (turn == 1 and stop2 == true) {
+            choice = 1;
+            turn = 0;
+        }
+        else {
+            cout << "Inserisci una scelta:\n    1 - Rimani fermo (non estrai numeri);\n    2 - Estrai numero casuale tra 5 e 10;\n    3 - Estrai due numeri casuali tra 1 e 5;\nScegli >> ";
+            cin >> choice;    
+        }
+        
         
         switch (choice) {
             default:
                 cout << "Scelta non riconosciuta! (" << choice << ").\n";
-                break;
+                        break;
             case 1:
                 cout << "Rimani fermo (passi).\n";
-                turn = 1;
+                if (turn == 0) {
+                    if (player1 > player2 and player1 <= 100) {
+                        wins = 0;
+                    }
+                    else if (player1 < player2 and player2 <= 100) {
+                        wins = 1;
+                    }
+                    turn = 1;
+                }
+                else if (turn == 1) {
+                    if ((player1 > player2 and player1 <= 100) or player2 > 100) {
+                        wins = 0;
+                    }
+                    else if ((player1 < player2 and player2 <= 100) or player1 > 100) {
+                        wins = 1;
+                    }
+                    turn = 0;
+                }
                 break;
             case 2:
                 cout << "Numero casuale tra 5 e 10.\n";
@@ -78,10 +109,10 @@ int main() {
         system("pause");
     }
 
-    if ((player1 <= 100 and player1 > player2) or (player1 <= 100 and player2 > 100)) {
+    if (wins == 0) {
         cout << "PLAYER 1 WINS!\n";
     }
-    else if ((player2 <= 100 and player2 > player1) or (player2 <= 100 and player1 > 100)) {
+    else if (wins == 1) {
         cout << "PLAYER 2 WINS!\n";
     }
 
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # schoolProjects
 My school projects. Usually in C++
+
+# Docs
+## Funzioni
+Funzione - insieme di istruzioni, racchiuse in un contenitore - Funzione - con un nome e che tornano un risultato (non sempre pero, dipende dal tipo della funzione).
+
+```cpp
+// Dichiarazione
+int calcoloPotenza(x, y) {
+    int ris = 0;
+
+    return ris
+}
+// La funzione ha: un tipo, un nome, dei parametri da mettere all'interno delle parentesi.
+// Chiamata di una funzione
+calcoloPotenza(1, 10);
+```
