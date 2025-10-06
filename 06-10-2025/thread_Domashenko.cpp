f3a9bef8db0c5e33848473b5fd24d2c07225c803
Quezka <arsdom15@gmail.com>
2025-10-06 13:30:20 +0200

    added thread

---
+++ 06-10-2025/thread_Domashenko.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <thread>
+#include <time.h>
+#include <chrono>
+using namespace std;
+
+int i, j;
+void runner1()
+{
+    for (i = 0; i < 20; i++)
+    {
+        cout << "\nrunner1: "
+             << i << "\n";
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+}
+
+void runner2()
+{
+    for (j = 0; j < 20; j++)
+    {
+        cout << "\nrunner2: "
+             << j << "\n";
+        this_thread::sleep_for(chrono::seconds(3));
+    }
+}
+
+int main()
+{
+    system("cls");
+
+    thread t1(runner1), t2(runner2);
+
+    t1.join();
+    t2.join();
+    system("pause");
+    return 0;
+}
