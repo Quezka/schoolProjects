fad985c5e74d60459edf7ef3f32a674c988d3386
Quezka <132306310+Quezka@users.noreply.github.com>
2025-10-08 09:03:05 +0200

    changed the threads project

--- 06-10-2025/thread_Domashenko.cpp
+++ 06-10-2025/thread_Domashenko.cpp
@@ -21,7 +21,7 @@ void runner2()
     {
         cout << "\nrunner2: "
              << j << "\n";
-        this_thread::sleep_for(chrono::seconds(3));
+        this_thread::sleep_for(chrono::seconds(1));
     }
 }
 
